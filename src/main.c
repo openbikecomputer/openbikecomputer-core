0fb8205dd0643b42baff2f5dd77eb14a6f853df3
LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>
2023-10-15 01:14:29 +0200

    main: Rework main to use the config
    Signed-off-by: LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>

--- src/main.c
+++ src/main.c
@@ -18,118 +18,38 @@
 
 #include <stdio.h>
 #include <unistd.h>
-
-#include "lvgl.h"
-#include "wayland.h"
-
-#include <limits.h>
-#include <errno.h>
-#include <poll.h>
-#include <pthread.h>
-
-#define SCREEN_HOR_SIZE (800)
-#define SCREEN_VER_SIZE (480)
-#define LVGL_REFRESH_TICK_RATE 30 /*milliseconds*/
-
-static void event_handler(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-
-    if(code == LV_EVENT_CLICKED) {
-        LV_LOG_USER("Clicked");
-    }
-    else if(code == LV_EVENT_VALUE_CHANGED) {
-        LV_LOG_USER("Toggled");
-    }
-}
-
-static void lv_example_btn_1(void)
-{
-    lv_obj_t * label;
-
-    lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
-    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
-
-    label = lv_label_create(btn1);
-    lv_label_set_text(label, "Button");
-    lv_obj_center(label);
-
-    lv_obj_t * btn2 = lv_btn_create(lv_scr_act());
-    lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
-    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-    lv_obj_set_height(btn2, LV_SIZE_CONTENT);
-
-    label = lv_label_create(btn2);
-    lv_label_set_text(label, "Toggle");
-    lv_obj_center(label);
-}
-
-static void * tick_thread_handler(void *data)
-{
-	while(1)
-	{
-		lv_tick_inc(LVGL_REFRESH_TICK_RATE);
-		usleep(LVGL_REFRESH_TICK_RATE * 1000);
-	}
-}
+#include "version.h"
+#include "ui.h"
+#include "log.h"
+#include "bike_config.h"
+#include "rider_config.h"
+#include "system_config.h"
 
 int main(int argc, char **argv)
 {
-	int ret;
-	struct pollfd pfd;
-	uint32_t time_till_next;
-	int sleep;
-	pthread_t tick_thread;
+	int ret = 0;
 
-	printf("OpenBikeComputer main application\n");
+	printf("OpenBikeComputer core V%d.%d.%d\n",\
+		OPEN_BIKE_COMPUTER_VERSION_MAJOR,
+		OPEN_BIKE_COMPUTER_VERSION_MINOR,
+		OPEN_BIKE_COMPUTER_VERSION_FIX
+	);
 
-	printf("Init LVGL\n");
-	lv_init();
-	printf("Init LVGL Wayland\n");
-	lv_wayland_init();
+	ret = system_config_init();
+	fail_if_negative(ret, -1, "Error: system_config_init failed, return: %d\n", ret);
 
-	/* Create a display */
-	printf("Create display\n");
-	lv_disp_t * disp = lv_wayland_create_window(SCREEN_HOR_SIZE, SCREEN_VER_SIZE, "openbikecomputer", NULL /*close_cb*/);
+	ret = bike_config_init();
+	fail_if_negative(ret, -2, "Error: bike_config_init failed, return: %d\n", ret);
 
-	printf("Set fullscreen mode\n");
-	lv_wayland_window_set_fullscreen(disp, true);
+	ret = rider_config_init();
+	fail_if_negative(ret, -3, "Error: rider_config_init failed, return: %d\n", ret);
 
-	printf("Draw example button on screen\n");
-	lv_example_btn_1();
+	ret = ui_init();
+	fail_if_negative(ret, -4, "Error: ui initialization failed, return: %d\n", ret);
 
-	printf("Manage LVGL timer event\n");
-	pfd.fd = lv_wayland_get_fd();
-	pfd.events = POLLIN;
-
-	ret = pthread_create(&tick_thread, NULL, &tick_thread_handler, NULL);
-	if(ret != 0)
-	{
-		printf("Create thread failed, return: %d\n", ret);
-		exit(-1);
-	}
-
+	/* Don't exit the application */
 	while (1) {
-		/* Handle any Wayland/LVGL timers/events */
-		time_till_next = lv_wayland_timer_handler();
-
-		/* Run until the last window closes */
-		if (!lv_wayland_window_is_open(NULL)) {
-			break;
-		}
-
-		/* Wait for something interesting to happen */
-		if (time_till_next == LV_NO_TIMER_READY) {
-			sleep = -1;
-		} else if (time_till_next > INT_MAX) {
-			sleep = INT_MAX;
-		} else {
-			sleep = time_till_next;
-		}
-
-		while ((poll(&pfd, 1, sleep) < 0) && (errno == EINTR));
+		pause();
 	}
 
 	return 0;
