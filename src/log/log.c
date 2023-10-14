b0f7b778d6d8008b4ef0dcec9306e4ef8ef23f7c
LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>
2023-10-14 22:51:23 +0200

    Add logging
    Signed-off-by: LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>

---
+++ src/log/log.c
@@ -0,0 +1,110 @@
+/*
+	OpenBikeComputer core application
+    Copyright (C) 2023  LAMBS Pierre-Antoine
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#include "log.h"
+
+#define _GNU_SOURCE
+#include <errno.h>
+extern char * program_invocation_short_name;
+
+#define DEFAULT_LOG_FOLDER "/tmp/"
+#define DEFAULT_LOG_FILE_NAME "log_file.log"
+#define LOG_FILE_PATH_MAX_LENGTH 256
+#define MAX_LOG_LENGTH 256
+
+//Log color
+#define NORMAL    "\x1B[0m"
+#define GREEN     "\x1B[32m"
+#define BLACKBOLD "\033[1m"
+#define REDBOLD   "\033[31m"
+
+struct{
+	bool use_color;
+} g_log = {
+	.use_color = false,
+};
+
+void _write_log(const char * filename, const char * function, int line, const char * format, ...){
+	int ret;
+	struct timeval tv;
+	struct tm * ptm;
+	va_list args;
+
+	char tmp_time_string[32];
+	char time_string[32];
+	char log_va_list_buffer[MAX_LOG_LENGTH];
+
+	/*
+	 * Get va_args into log_va_list_buffer
+	 */
+	va_start( args,format);
+	vsnprintf( log_va_list_buffer, sizeof(log_va_list_buffer), format, args);
+	va_end( args);
+
+	/*
+	 * make string from time
+	 */
+	ret = gettimeofday( &tv, NULL);
+	if( ret < 0) {
+		printf( "Error gettimeofday return %d\n", ret);
+	}
+
+	ptm = localtime( &tv.tv_sec);
+	ret = strftime( tmp_time_string, sizeof(tmp_time_string), "%Y-%m-%d %H:%M:%S", ptm);
+	if( ret == 0) {
+		printf( "Error strftime return %d\n", (int) ret);
+	}
+
+	ret = snprintf( time_string, sizeof( time_string), "%s.%03ld", tmp_time_string, (tv.tv_usec/1000));
+	if( ret < 0) {
+		printf( "Error snprintf return %d\n", ret);
+	}
+
+	/*
+	 * Print log on console
+	 */
+	if( g_log.use_color == true ){
+		printf( "%s "GREEN" %s [%d:%d]"NORMAL" ["BLACKBOLD"%s:%d:%s"NORMAL"] "REDBOLD"%s"NORMAL, time_string, program_invocation_short_name, getpid(),(int)syscall( SYS_gettid), filename, line, function, log_va_list_buffer);
+	}else{
+		printf( "%s %s [%d:%d] [%s:%d:%s] %s", time_string, program_invocation_short_name,getpid(),(int)syscall( SYS_gettid), filename, line, function, log_va_list_buffer);
+	}
+
+	return;
+}
+
+int log_set_color(bool value){
+	g_log.use_color = value;
+
+	log_info("Set log color: %s\n", value ? "ON" : "OFF");
+	return 0;
+}
---
+++ src/log/log.h
@@ -0,0 +1,209 @@
+/*
+	OpenBikeComputer core application
+    Copyright (C) 2023  LAMBS Pierre-Antoine
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LOG_HEADER_
+#define _LOG_HEADER_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define LOG_LEVEL_ERROR 3
+#define LOG_LEVEL_WARN 2
+#define LOG_LEVEL_INFO 1
+#define LOG_LEVEL_DEBUG 0
+
+/* Define default log level */
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_LEVEL_ERROR
+#endif
+
+int log_set_color( bool value);
+
+// --- LOG ---
+//#define log(...)
+//#define log_error(...)
+//#define log_warn(...)
+//#define log_info(...)
+//#define log_debug(...)
+
+// --- Common die ---
+//#define fail(return_value, ...)
+//#define fail_if_negative_or_zero(value_to_check, return_value, ...)
+//#define fail_if_negative(value_to_check, return_value, ...)
+//#define fail_if_zero(value_to_check, return_value, ...)
+//#define fail_if_not_zero(value_to_check, return_value, ...)
+//#define fail_if_positive(value_to_check, return_value, ...)
+//#define fail_if_positive_or_zero(value_to_check, return_value, ...)
+//#define fail_if_null(value_to_check, return_value, ...)
+//#define fail_if_not_null(value_to_check, return_value, ...)
+//#define fail_if_true(value_to_check, return_value, ...)
+//#define fail_if_false(value_to_check, return_value, ...)
+//#define fail_if_equal(value_to_check_1, value_to_check_2, return_value, ...)
+//#define fail_if_not_equal(value_to_check_1, value_to_check_2, return_value, ...)
+//#define fail_if_superior(value_to_check_1, value_to_check_2, return_value, ...)
+//#define fail_if_inferior(value_to_check_1, value_to_check_2, return_value, ...)
+//#define fail_if_superior_or_equal(value_to_check_1, value_to_check_2, return_value, ...)
+//#define fail_if_inferior_or_equal(value_to_check_1, value_to_check_2, return_value, ...)
+
+//_write_log() function don't use it directy, use log() macro instead
+void _write_log(const char * filename, const char * function, int line, const char * format, ...);
+
+//#########################################
+//#                                       #
+//#               Log section             #
+//#                                       #
+//#########################################
+
+
+#define log(level, ...) do { if(level >= LOG_LEVEL) { _write_log(__FILE__, __FUNCTION__, __LINE__, __VA_ARGS__); } } while(0)
+
+/* Write simple log */
+#define log_error(...) log(LOG_LEVEL_ERROR, __VA_ARGS__)
+#define log_warn(...)  log(LOG_LEVEL_WARN, __VA_ARGS__)
+#define log_info(...)  log(LOG_LEVEL_INFO, __VA_ARGS__)
+#define log_debug(...) log(LOG_LEVEL_DEBUG, __VA_ARGS__)
+
+//#########################################
+//#                                       #
+//#           Common die section          #
+//#                                       #
+//#########################################
+
+/*
+ * DIE IF THE VALUE IS EXPECTED
+ */
+
+//Just DIE...
+#define fail_if(return_value, ...){                                        \
+	log_error(__VA_ARGS__);                                                   \
+	return return_value;                                                      \
+}
+
+//Die if value_to_check is 'Negative' OR 'zero'
+#define fail_if_negative_or_zero(value_to_check, return_value, ...)        \
+	if(value_to_check <= 0){                                                  \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is stricly 'Negative'
+#define fail_if_negative(value_to_check, return_value, ...)                \
+	if(value_to_check < 0){                                                   \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is stricly 'Zero'
+#define fail_if_zero(value_to_check, return_value, ...)                    \
+	if(value_to_check == 0){                                                  \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is stricly different from 'Zero'
+#define fail_if_not_zero(value_to_check, return_value, ...)                \
+	if(value_to_check != 0){                                                  \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is stricly 'Positive'
+#define fail_if_positive(value_to_check, return_value, ...)                \
+	if(value_to_check > 0){                                                   \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is 'Positive' OR 'Zero'
+#define fail_if_positive_or_zero(value_to_check, return_value, ...)        \
+	if(value_to_check >= 0){                                                  \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is 'NULL'
+#define fail_if_null(value_to_check, return_value, ...)                    \
+	if(value_to_check == NULL){                                               \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is 'NOT NULL'
+#define fail_if_not_null(value_to_check, return_value, ...)                \
+	if(value_to_check != NULL){                                               \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is 'TRUE'
+#define fail_if_true(value_to_check, return_value, ...)                    \
+	if(value_to_check == true){                                               \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check is 'FALSE'
+#define fail_if_false(value_to_check, return_value, ...)                   \
+	if(value_to_check == false){                                              \
+		log_error(__VA_ARGS__);                                               \
+		return return_value;                                                  \
+	}
+
+//Die if value_to_check_1 and value_to_check_2 are 'EQUAL'
+#define fail_if_equal(value_to_check_1, value_to_check_2, return_value, ...)    \
+	if(value_to_check_1 == value_to_check_2){                                      \
+		log_error(__VA_ARGS__);                                                    \
+		return return_value;                                                       \
+	}
+
+//Die if value_to_check_1 and value_to_check_2 are 'NOT EQUAL'
+#define fail_if_not_equal(value_to_check_1, value_to_check_2, return_value, ...)\
+	if(value_to_check_1 != value_to_check_2){                                      \
+		log_error(__VA_ARGS__);                                                    \
+		return return_value;                                                       \
+	}
+
+//Die if value_to_check_1 is 'SUPERIOR TO' value_to_check_2
+#define fail_if_superior(value_to_check_1, value_to_check_2, return_value, ...) \
+	if(value_to_check_1 > value_to_check_2){                                       \
+		log_error(__VA_ARGS__);                                                    \
+		return return_value;                                                       \
+	}
+
+//Die if value_to_check_1 is 'INFERIOR TO' value_to_check_2
+#define fail_if_inferior(value_to_check_1, value_to_check_2, return_value, ...) \
+	if(value_to_check_1 < value_to_check_2){                                       \
+		log_error(__VA_ARGS__);                                                    \
+		return return_value;                                                       \
+	}
+
+//Die if value_to_check_1 is 'SUPERIOR OR EQUAL TO' value_to_check_2
+#define fail_if_superior_or_equal(value_to_check_1, value_to_check_2, return_value, ...)\
+	if(value_to_check_1 >= value_to_check_2){                                              \
+		log_error(__VA_ARGS__);                                                            \
+		return return_value;                                                               \
+	}
+
+//Die if value_to_check_1 is 'INFERIOR OR EQUAL TO' value_to_check_2
+#define fail_if_inferior_or_equal(value_to_check_1, value_to_check_2, return_value, ...)\
+	if(value_to_check_1 <= value_to_check_2){                                              \
+		log_error(__VA_ARGS__);                                                            \
+		return return_value;                                                               \
+	}
+
+#endif //_LOG_HEADER_
