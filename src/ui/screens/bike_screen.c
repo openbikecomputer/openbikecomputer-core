81833dac12fa4f19b0cbcf5d1e00d621c3e82a5d
LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>
2024-08-27 22:41:00 +0200

    feat: update bike_screen menu layout
    Signed-off-by: LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>

--- src/ui/screens/bike_screen.c
+++ src/ui/screens/bike_screen.c
@@ -25,6 +25,8 @@
 #include "system.h"
 #include "bike_screen.h"
 #include "ui.h"
+#include "locales.h"
+#include "styles.h"
 
 static struct {
 	uint32_t btn_cnt;
@@ -34,128 +36,213 @@ static struct {
 	.btn_cnt = 1,
 };
 
-#define PCT_HOR_SIZE 45
-#define PCT_VER_SIZE (100/6)
-static void _create_subpage(void)
+typedef struct {
+	lv_obj_t *container;
+	lv_obj_t *label;
+	lv_obj_t *data;
+} T_bike_menu_element;
+
+#define LABEL_GAP_PX 15
+#define OFFSET_Y 0
+#define PCT_HOR_SIZE 50
+#define KEYBOARD_HEIGHT 40
+#define NB_ENTRY 6
+#define PCT_VER_SIZE ((100-KEYBOARD_HEIGHT)/NB_ENTRY)
+
+static void _ta_event_cb(lv_event_t * e)
 {
-    lv_obj_t *sub_page, *cont, *label, *ta, *dd, *kb;
+	lv_event_code_t code = lv_event_get_code(e);
+	lv_obj_t * ta = lv_event_get_target(e);
+	lv_obj_t * kb = lv_event_get_user_data(e);
+	if(code == LV_EVENT_FOCUSED) {
+		lv_obj_add_state(ta, LV_STATE_FOCUSED);
+		lv_keyboard_set_textarea(kb, ta);
+	}
 
-	printf("Create subpage: %d\n", bike_screen.btn_cnt);
+	if(code == LV_EVENT_DEFOCUSED) {
+		lv_obj_remove_state(ta, LV_STATE_FOCUSED);
+		lv_keyboard_set_textarea(kb, NULL);
+	}
+}
 
-	/* Create the menu subpage */
-    sub_page = lv_menu_page_create(bike_screen.menu, NULL);
+static int _create_text_entry(T_bike_menu_element *element, lv_obj_t *subpage, char *name, char *placeholder)
+{
+	fail_if_null(element, -1, "element is NULL\n");
+	fail_if_null(subpage, -2, "subpage is NULL\n");
+	fail_if_null(name, -3, "name is NULL\n");
+	fail_if_null(placeholder, -4, "placeholder is NULL\n");
 
-	/* Populate subpage content */
-	/* Name */
-    cont = lv_menu_cont_create(sub_page);
-    label = lv_label_create(cont);
-    lv_label_set_text_fmt(label, "Name:");
-    //lv_obj_set_size(label, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+	/* Create container */
+	element->container = lv_menu_cont_create(subpage);
+	fail_if_null(element->container, -6, "lv_label_create failed\n");
 
-	/* Name text area writable using the keyboard */
-    ta = lv_textarea_create(cont);
-    //lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, kb);
-    lv_textarea_set_placeholder_text(ta, "Jean-Michel");
-	lv_obj_set_width(ta, lv_pct(PCT_HOR_SIZE));
-    //lv_obj_set_size(ta, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(ta, LV_ALIGN_RIGHT_MID, 0, 0);
+	/* Apply style */
+	lv_obj_add_style(element->container, styles_get_no_border_style(), LV_PART_MAIN | LV_STATE_DEFAULT);
 
-	/* Brand */
-    cont = lv_menu_cont_create(sub_page);
-    label = lv_label_create(cont);
-    lv_label_set_text_fmt(label, "Brand:");
-    //lv_obj_set_size(label, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+	/* Set container size */
+    lv_obj_set_size(element->container, lv_pct(100), lv_pct(PCT_VER_SIZE));
+	lv_obj_update_layout(element->container);
 
-	/* Name text area writable using the keyboard */
-    ta = lv_textarea_create(cont);
-    //lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, kb);
-    lv_textarea_set_placeholder_text(ta, "AvalanceBike");
-	lv_obj_set_width(ta, lv_pct(PCT_HOR_SIZE));
-    //lv_obj_set_size(ta, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(ta, LV_ALIGN_RIGHT_MID, 0, 0);
+	/* Create label in container */
+    element->label = lv_label_create(element->container);
+	fail_if_null(element->label, -7, "lv_label_create failed\n");
 
-	/* Model */
-    cont = lv_menu_cont_create(sub_page);
-    label = lv_label_create(cont);
-    lv_label_set_text_fmt(label, "Model:");
-    //lv_obj_set_size(label, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_label_set_text(element->label, name);
+    lv_obj_set_size(element->label, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+	lv_obj_update_layout(element->label);
 
-	/* Model text area writable using the keyboard */
-    ta = lv_textarea_create(cont);
-    //lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, kb);
-    lv_textarea_set_placeholder_text(ta, "Topbike3000");
-	lv_obj_set_width(ta, lv_pct(PCT_HOR_SIZE));
-    //lv_obj_set_size(ta, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(ta, LV_ALIGN_RIGHT_MID, 0, 0);
+	/* Create text area in container */
+    element->data = lv_textarea_create(element->container);
+	fail_if_null(element->data, -8, "lv_textarea_create failed\n");
 
-	/* Weight */
-    cont = lv_menu_cont_create(sub_page);
-    label = lv_label_create(cont);
-    lv_label_set_text_fmt(label, "Weight:");
-    //lv_obj_set_size(label, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_textarea_set_placeholder_text(element->data, placeholder);
+    lv_obj_set_size(element->data, lv_pct(PCT_HOR_SIZE), lv_pct(100));
+	lv_obj_update_layout(element->data);
 
-	/* Weight text area writable using the keyboard */
-    ta = lv_textarea_create(cont);
-    //lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, kb);
-    lv_textarea_set_placeholder_text(ta, "15kg");
-	lv_obj_set_width(ta, lv_pct(PCT_HOR_SIZE));
-    //lv_obj_set_size(ta, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(ta, LV_ALIGN_RIGHT_MID, 0, 0);
+	/* Align the label and the text area */
+    lv_obj_align(element->label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_align_to(element->data, element->label, LV_ALIGN_OUT_RIGHT_MID, LABEL_GAP_PX, OFFSET_Y);
 
-	/* Type */
-    cont = lv_menu_cont_create(sub_page);
-    label = lv_label_create(cont);
-    lv_label_set_text_fmt(label, "Type:");
-    //lv_obj_set_size(label, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+	return 0;
+}
 
-	/*Create bike type drop down list*/
-    dd = lv_dropdown_create(cont);
-    lv_dropdown_set_options(dd, "All terrain bike\n"
-                            "Mountain bike\n"
-                            "Road\n"
-                            "Gravel\n"
-                            "Touring bike\n"
-                            "Speedbike\n"
-                            "Other");
+#define DROP_DOWN_STR_LENGTH 256
+static int _get_drop_down_translations(char *str, int length)
+{
+	int ret = 0;
+    ret = snprintf(str, length, "%s\n%s\n%s\n%s\n%s\n%s\n%s",
+					_("All terrain bike"),
+                    _("Mountain bike"),
+                    _("Road"),
+                    _("Gravel"),
+                    _("Touring bike"),
+                    _("Speedbike"),
+                    _("Other"));
+	if(ret >= length)
+	{
+		log_error("str was truncated\n");
+		/* Force the \0 char when the string is truncated */
+		str[DROP_DOWN_STR_LENGTH - 1] = '\0';
+		return -1;
+	}
 
-	lv_obj_set_width(dd, lv_pct(PCT_HOR_SIZE));
-    //lv_obj_set_size(dd, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(dd, LV_ALIGN_RIGHT_MID, 0, 0);
-    //lv_obj_add_event_cb(dd, event_handler, LV_EVENT_ALL, NULL);
+	return 0;
+}
 
-	/* Wheel size */
-    cont = lv_menu_cont_create(sub_page);
-    label = lv_label_create(cont);
-    lv_label_set_text_fmt(label, "Whell size:");
-    //lv_obj_set_size(label, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+static int _create_drop_down_entry(T_bike_menu_element *element, lv_obj_t *subpage)
+{
+	char str[DROP_DOWN_STR_LENGTH];
+	int ret = 0;
 
-	/* Wheel size text area writable using the keyboard */
-    ta = lv_textarea_create(cont);
-    //lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, kb);
-    lv_textarea_set_placeholder_text(ta, "2298mm");
-	lv_obj_set_width(ta, lv_pct(PCT_HOR_SIZE));
-    //lv_obj_set_size(ta, lv_pct(PCT_HOR_SIZE), lv_pct(PCT_VER_SIZE));
-    lv_obj_align(ta, LV_ALIGN_RIGHT_MID, 0, 0);
+	fail_if_null(subpage, -1, "subpage is NULL\n");
+	fail_if_null(element, -2, "element is NULL\n");
+
+	/* Create container */
+	element->container = lv_obj_create(subpage);
+	fail_if_null(element->container, -3, "container is NULL\n");
+
+	/* Apply style */
+	lv_obj_add_style(element->container, styles_get_no_border_style(), LV_PART_MAIN | LV_STATE_DEFAULT);
+
+	/* Set container size */
+    lv_obj_set_size(element->container, lv_pct(100), lv_pct(PCT_VER_SIZE));
+	lv_obj_update_layout(element->container);
+
+	/* Create label in container */
+    element->label = lv_label_create(element->container);
+    lv_label_set_text(element->label, _("Type:"));
+    lv_obj_set_size(element->label, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+	lv_obj_update_layout(element->label);
+
+	/*Create drop down list in container */
+    element->data = lv_dropdown_create(element->container);
+	ret = _get_drop_down_translations(str, DROP_DOWN_STR_LENGTH);
+	if(ret < 0)
+	{
+		log_error("_get_drop_down_translations failed, returned %d\n", ret);
+	}
+    lv_dropdown_set_options(element->data, str);
+    lv_obj_set_size(element->data, lv_pct(PCT_HOR_SIZE), lv_pct(100));
+	lv_obj_update_layout(element->data);
+
+	/* Align the label and the drop down list */
+    lv_obj_align(element->label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_align_to(element->data, element->label, LV_ALIGN_OUT_RIGHT_MID, LABEL_GAP_PX, OFFSET_Y);
+
+	return 0;
+}
+
+static lv_obj_t* _create_keyboard(lv_obj_t *subpage)
+{
+	lv_obj_t *kb;
+
+	fail_if_null(subpage, NULL, "subpage is NULL\n");
 
 	/* Add the keyboard to enter the informations */
-	kb = lv_keyboard_create(sub_page);
-    lv_obj_set_size(kb, lv_pct(100), lv_pct(50));
+	kb = lv_keyboard_create(subpage);
+    lv_obj_set_size(kb, lv_pct(100), lv_pct(KEYBOARD_HEIGHT));
     lv_obj_align(kb, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+	return kb;
+}
+
+static int _create_subpage(void)
+{
+	int ret = 0;
+    lv_obj_t *subpage, *cont, *label, *kb;
+	T_bike_menu_element menu_entry[NB_ENTRY] = {0};
+
+	log("Create subpage: %d\n", bike_screen.btn_cnt);
+
+	/* Create the menu subpage */
+    subpage = lv_menu_page_create(bike_screen.menu, NULL);
+	fail_if_null(subpage, -1, "subpage is NULL\n");
+
+	/* Populate subpage content in display order */
+	ret = _create_text_entry(&menu_entry[0], subpage, _("Name:"), _("Bike name"));
+	fail_if_negative(ret, -2, "_create_text_entry failed, return: %d\n", ret);
+
+	ret = _create_text_entry(&menu_entry[1], subpage, _("Brand:"), _("Bike brand"));
+	fail_if_negative(ret, -3, "_create_text_entry failed, return: %d\n", ret);
+
+	ret = _create_text_entry(&menu_entry[2], subpage, _("Model:"), _("Bike model"));
+	fail_if_negative(ret, -4, "_create_text_entry failed, return: %d\n", ret);
+
+	ret = _create_text_entry(&menu_entry[3], subpage, _("Weight:"), _("Bike weight in Kg"));
+	fail_if_negative(ret, -5, "_create_text_entry failed, return: %d\n", ret);
+
+	ret = _create_drop_down_entry(&menu_entry[4], subpage);
+	fail_if_negative(ret, -6, "_create_drop_down_entry failed, return: %d\n", ret);
+
+	ret = _create_text_entry(&menu_entry[5], subpage, _("Wheel size:"), _("Bike wheel size"));
+	fail_if_negative(ret, -7, "_create_text_entry failed, return: %d\n", ret);
+
+	/* Create the keyboard */
+	kb = _create_keyboard(subpage);
+	fail_if_null(kb, -8, "kb is NULL\n");
+
+	/* Add keyboard event callback */
+    lv_obj_add_event_cb(menu_entry[0].data, _ta_event_cb, LV_EVENT_ALL, kb);
+    lv_obj_add_event_cb(menu_entry[1].data, _ta_event_cb, LV_EVENT_ALL, kb);
+    lv_obj_add_event_cb(menu_entry[2].data, _ta_event_cb, LV_EVENT_ALL, kb);
+    lv_obj_add_event_cb(menu_entry[3].data, _ta_event_cb, LV_EVENT_ALL, kb);
+	/* The entry 4 with the drop down doesn't need keyboard input */
+    lv_obj_add_event_cb(menu_entry[5].data, _ta_event_cb, LV_EVENT_ALL, kb);
+
+	/* Put the focus on the first element */
+	lv_obj_add_state(menu_entry[0].data, LV_STATE_FOCUSED);
+	lv_keyboard_set_textarea(kb, menu_entry[0].data);
 
 	/* Update the main page to add the subpage entry */
     cont = lv_menu_cont_create(bike_screen.main_page);
     label = lv_label_create(cont);
-    lv_label_set_text_fmt(label, "New bike %"LV_PRIu32, bike_screen.btn_cnt);
-    lv_menu_set_load_page_event(bike_screen.menu, cont, sub_page);
+    lv_label_set_text_fmt(label, "New bike%"LV_PRIu32, bike_screen.btn_cnt);
+    lv_menu_set_load_page_event(bike_screen.menu, cont, subpage);
 
 	/* Scroll to the new entry on the front page, this make it visible */
     lv_obj_scroll_to_view_recursive(cont, LV_ANIM_ON);
+
+	return 0;
 }
 
 static void _float_button_handler(lv_event_t * e)
@@ -190,15 +277,15 @@ int bike_screen_enter(lv_obj_t *screen)
 
 	/* Set size and center the menu */
     lv_obj_set_size(bike_screen.menu, lv_pct(100), lv_pct(100));
-    lv_obj_center(bike_screen.menu);
+    lv_obj_align(bike_screen.menu, LV_ALIGN_TOP_MID, 0, 0);
 
-	printf("Create menu main page\n");
+	log("Create menu main page\n");
     /*Create a main page that will contain the bike list */
     bike_screen.main_page = lv_menu_page_create(bike_screen.menu, NULL);
 
 	/* TODO Populate the menu with existing bike configuration */
 
-	printf("Create menu button\n");
+	log("Create menu button\n");
     /* Create floating button to add new bike configuration  */
     lv_obj_t * float_btn = lv_button_create(bike_screen.main_page);
 	int btn_size = (ui_get_resolution_hor()/4);
@@ -209,11 +296,10 @@ int bike_screen_enter(lv_obj_t *screen)
     lv_obj_set_style_radius(float_btn, LV_RADIUS_CIRCLE, 0);
     lv_obj_set_style_bg_img_src(float_btn, LV_SYMBOL_PLUS, 0);
 
-	printf("Create menu button\n");
+	log("Create menu button\n");
 	/* Go to the main page */
     lv_menu_set_page(bike_screen.menu, bike_screen.main_page);
 
-	printf("done\n");
 	return 0;
 }
 
